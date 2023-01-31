8331c92410052f222a44d0245702a33440d9b400
alvarosopena <alvarosopenaf@gmail.com>
2023-01-31 19:29:48 -0300

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda = "../assets/dice1.png";
+  dadoDerecha ="../assets/dice2.png";
+  numero1: number = 1; 
+  numero2: number = 2;
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random()*5)+1;
+    this.numero2 = Math.round(Math.random()*5)+1;
+    this.dadoIzquierda="../assets/dice"+this.numero1+".png";
+    this.dadoDerecha="../assets/dice"+this.numero2+".png";
+   // console.log(this.numero1);
+   // console.log(this.numero2);
+  }
 }
